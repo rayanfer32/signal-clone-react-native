e74a09d47cd98e19868b422796a9018891577b62
Rayan fernandes <37145078+Rayanfer32@users.noreply.github.com>
2021-04-07 11:06:02 +0530

    added scrollview to register screen

--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -1,14 +1,74 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useState} from 'react'
+import { KeyboardAvoidingView, ScrollView, StyleSheet, View } from 'react-native'
+import Text from "../components/Text"
+import { StatusBar } from "expo-status-bar"
+import { Button, Input, Image} from "react-native-elements"
 
 const RegisterScreen = ({ navigation }) => {
+    
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [imageUrl, setImageUrl] = useState("")
+
+    const register = () => {
+
+    }
+
     return (
-        <View>
-            <Text>This is Register screen</Text>
-        </View>
+        
+        <ScrollView style={styles.scrollView}> 
+        <KeyboardAvoidingView behavior="padding" style={styles.container} scrollable>
+            
+            <StatusBar style="light"/>
+            <Image style={{ width:150, height: 150 }}
+                source={{uri: "https://crypto.nexus.io/images/branding/GlobeBlue1000.png"}}
+            />
+            <Text h3 style={{ marginBottom: 50 }}>
+                Create Signal Account
+            </Text>
+
+            <View style={styles.inputContainer}>
+                <Input placeholder="Full Name" autofocus type="text" value={name}
+                    onChangeText={(text) => setName(text)}
+                />
+                <Input placeholder="Email" type="email" value={email}
+                    onChangeText={(text) => setEmail(text)}
+                />
+                <Input placeholder="Password" secureTextEntry type="password" value={password}
+                    onChangeText={(text) => setPassword(text)}
+                />
+                <Input placeholder="Profile Image Url (optional)" type="url" value={imageUrl}
+                    onChangeText={(text) => setImageUrl(text)}
+                />
+            </View>
+
+            <Button containerStyle={styles.button} onPress={register} title="Register" />
+            {/* <View style={{ height: 100}} /> */}
+            
+        </KeyboardAvoidingView>
+        </ScrollView>
     )
 }
 
 export default RegisterScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        padding: 10,
+        backgroundColor: "white",
+    },
+    scrollView:{
+        // marginHorizontal: 20,
+    },
+    inputContainer: {
+        width: 300,
+    },
+    button: {
+        width: 200,
+        margin: 10,
+    }
+})
